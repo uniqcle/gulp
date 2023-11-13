76452c806dfc5cec3b5867287a81008f12ccd094
uniqcle <uniqcle@yandex.ru>
2023-11-13 16:47:46 +0700

    clear dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 const gulp = require("gulp");
 const fileInclude = require("gulp-file-include");
 const sass = require("gulp-sass")(require("sass"));
+const clean = require("gulp-clean");
+const fs = require("fs");
+
+////////////////////////////////////////
+// clear dist folder
+////////////////////////////////////////
+gulp.task("clear", function (done) {
+  if (fs.existsSync("./dist/")) {
+    return gulp.src("./dist/", { read: false }).pipe(clean({ force: true }));
+  }
+});
 
 ////////////////////////////////////////
 // индклудинг html файлов
