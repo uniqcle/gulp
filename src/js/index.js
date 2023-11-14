541973b5eda2c1e1713ac1f38943c2a31e42a120
uniqcle <uniqcle@yandex.ru>
2023-11-14 16:20:15 +0700

    date picker

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
 import hello from "./modules/hello";
 
+import AirDatepicker from "air-datepicker";
+import "air-datepicker/air-datepicker.css";
+
+new AirDatepicker("#date");
+
+
 console.log(" index js...");
 console.log(hello);
